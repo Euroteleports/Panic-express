a7e56418b1e1f1f15ce15e947f8c73cbd8625e72
Anthony-Noel <anthony.noel117@gmail.com>
2026-06-03 17:40:09 +0200

    test animation

---
+++ Assets/AnimationController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Debug.Log(animator);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            Debug.Log("Walking");
+            animator.SetBool("is walking", true);
+        }
+
+        if (!Input.GetKey("w"))
+        {
+            Debug.Log("Walking");
+            animator.SetBool("is walking", false);
+        }
+    }
+}
