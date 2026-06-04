8a17ae5088a3a9a85d647bd513f74b8d62e41f5f
Anthony-Noel <anthony.noel117@gmail.com>
2026-06-04 02:03:35 +0200

    test transition anime

--- Assets/AnimationController.cs
+++ Assets/AnimationController.cs
@@ -5,27 +5,61 @@ using UnityEngine;
 public class AnimationController : MonoBehaviour
 {
     Animator animator;
+    int isWalkingHash;
+    int isRunningHash;
+
+    float velocity = 0.0f;
+    [SerializeField] float acceleration = 0.1f;
+    [SerializeField] float decceleration = 0.5f;
+    int VelocityHash;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
-        Debug.Log(animator);
+        isWalkingHash = Animator.StringToHash("is walking");
+        isRunningHash = Animator.StringToHash("is running");
+
+        VelocityHash = Animator.StringToHash("Velocity");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("w"))
+        bool isRunning = animator.GetBool(isRunningHash);
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+
+        if (/*!isWalking && */ forwardPressed && velocity < 1.0f)
         {
-            Debug.Log("Walking");
-            animator.SetBool("is walking", true);
+            velocity += Time.deltaTime * acceleration;
+            //animator.SetBool(isWalkingHash, true);
         }
 
-        if (!Input.GetKey("w"))
+        if (/*isWalking && */ !forwardPressed && velocity > 0.0f)
         {
-            Debug.Log("Walking");
-            animator.SetBool("is walking", false);
+            velocity -= Time.deltaTime * decceleration;
+            //animator.SetBool(isWalkingHash, false);
         }
+
+        if (/*!isWalking && */!forwardPressed && velocity < 0.0f)
+        {
+            velocity = 0.0f;
+            //animator.SetBool(isWalkingHash, false);
+        }
+
+
+        /*if (!isRunning && (forwardPressed && runPressed))
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+
+         if (isRunning && (!forwardPressed || !runPressed))
+        {
+            animator.SetBool(isRunningHash, false);
+        }*/
+
+        animator.SetFloat(VelocityHash, velocity);
     }
 }
