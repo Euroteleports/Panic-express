7b10e2dd536b5353d630082ee10097883e33f454
Anthony-Noel <anthony.noel117@gmail.com>
2026-06-05 13:48:42 +0200

    anim + mouv

--- Assets/Script/TankController.cs
+++ Assets/Script/TankController.cs
@@ -6,18 +6,33 @@ public class TankController : MonoBehaviour
 {
     public float moveSpeed = 4f;
     public float rotationSpeed = 150f;
-    private AudioSource Audiosource;
     private CharacterController controller;
+
+    private AudioSource audioSource;
+
+    int walking;
+    int running;
+    int turning;
+    float velocity;
+    [SerializeField] private Animator animator;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        Audiosource = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
+
+        walking = Animator.StringToHash("is walking");
+        running = Animator.StringToHash("is running");
+        turning = Animator.StringToHash("is turning");
+
+        velocity = Animator.StringToHash("is velocity");
     }
 
     void Update()
     {
         float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
         // Gère la rotation (Gauche / Droite)
         float turn = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
         transform.Rotate(0, turn, 0);
@@ -31,22 +46,46 @@ public class TankController : MonoBehaviour
         Vector3 movement = transform.forward * move;
         movement.y -= 9.81f * Time.deltaTime; 
 
-         
 
         controller.Move(movement);
 
-if (verticalInput != 0)
-    {
-        if (!Audiosource.isPlaying)
+
+        if (verticalInput != 0)
         {
-            Audiosource.Play();
+           
+            if (animator != null)
+            {
+                animator.SetBool(walking, true);
+            }
+
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
-    }
-    else
-    {
-        Audiosource.Stop();
-    }
+        else
+        {
+            animator.SetBool(walking, false);
+            audioSource.Stop();
+        }
 
+        /*if (horizontalInput != 0)
+        {
+            Debug.Log("Turning");
+            if (animator != null)
+            {
+                animator.SetBool(turning, true);
+            }
 
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            animator.SetBool(turning, false);
+            audioSource.Stop();
+        }*/
     }
 }
