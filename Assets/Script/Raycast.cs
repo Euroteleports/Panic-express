85850e81ba1e28270067f035293ebcf29bc0b63c
Euroteleport <lvl7singed@gmail.com>
2026-06-06 23:46:27 +0200

    Raycast fini
    Ca était dur mazète !

--- Assets/Script/Raycast.cs
+++ Assets/Script/Raycast.cs
@@ -4,33 +4,61 @@ using UnityEngine;
 
 public class Raycast : MonoBehaviour
 {
-Ray _ray;
-private RaycastHit _hit;
-public GameObject Cam;
+private Ray _ray;
+    private RaycastHit _hit;
+    public Transform Cam;
+    public bool estVu = false;
 
+    // VARIABLE POUR STOCKER LA HAUTEUR INITIALE
+    private float _hauteurInitiale;
 
-
-  void Update()
-  {
-
-    //C'est pour l'optimisation
-    _ray = new Ray(transform.position, transform.forward);
-
-    // Trace le rayon
-    Debug.DrawRay(_ray.origin, _ray.direction * 100f, Color.red);
-    
-    if(Physics.Raycast(_ray, out _hit, 100f))
+    void Start()
     {
-       if(_hit.collider.CompareTag("Respawn"))
-       {
-        Debug.Log("Observateur !");
-        Vector3 positionActuelle = Cam.transform.position;
-        positionActuelle.y = 4f; // Par exemple, monte à 10 unités de hauteur
-        Cam.transform.position = positionActuelle;
-       }
+        // Au lancement du jeu, on mémorise la vraie hauteur de ta caméra
+        if (Cam != null)
+        {
+            _hauteurInitiale = Cam.transform.position.y;
+        }
     }
-  }
+
+    void Update()
+    {
+        _ray = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(_ray.origin, _ray.direction * 100f, Color.red);
+        
+        if (Physics.Raycast(_ray, out _hit, 100f))
+        {
+            if (_hit.collider.CompareTag("Respawn"))
+            {
+                estVu = true;
+            } 
+            else
+            {
+                estVu = false;
+            }
+        }
+        else
+        {
+            estVu = false;
+        }
+
+        // --- DEPLACEMENT ---
+        Vector3 nouvellePos = Cam.transform.position;
+
+        if (estVu)
+        {
+            nouvellePos.y = 4f; // La hauteur quand elle se baisse
+        } 
+        else 
+        {
+            // Au lieu d'un chiffre fixe, on lui redonne sa VRAIE hauteur de départ
+            nouvellePos.y = _hauteurInitiale; 
+        }
+
+        Cam.transform.position = nouvellePos;
+    }
 }
+
 
 /* Donc l'objectif etant de detecter le tag de l'observateur, je creer donc un raycact qui se tyraduit par
    "Physics.Raycast()" en parametre on met sa position, la ou il regarde (d'ou le "forward").
